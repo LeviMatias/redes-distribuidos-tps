e8792aa677196ae02f7148c1594688bad8dbe8ff
axelmpm <axelmpm@gmail.com>
2021-05-02 20:00:36 -0300

    Se hace una primera version del cliente con funcionalidad minima

---
+++ tp1/client-tcp.py
@@ -0,0 +1,44 @@
+import os
+import atexit
+import socket
+from threading import Thread
+
+CHUNCK_SIZE = 16
+
+class Client:
+
+    def __init__(self,addres_family=socket.AF_INET,protocol=socket.SOCK_STREAM):
+        self.sock = socket.socket(addres_family,protocol)
+
+    def connect(self,host,port):
+        sock.connect({host,port})
+
+    def send(self,file_path):
+
+        f = open(file_path,'rb')
+        f.seek(0,os.SEEK_END)
+        size = f.tell()
+        f.seek(0,os.SEEK_SET)
+
+        sock.send(str(size).encode())
+
+        while True:
+            chunk = f.read(CHUNCK_SIZE)
+            sock.send(chunk)
+
+        f.close()
+
+    def close():
+        sock.close()
+
+
+if __name__ == "__main__":
+    
+    client = Client()
+
+    host = '127.0.0.1'
+    port = '8080'
+
+    client.connect(host,port)
+    client.send('C:\Users\axelpm\Desktop\test.txt')
+    client.close()
