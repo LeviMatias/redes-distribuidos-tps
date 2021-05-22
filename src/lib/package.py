917a0b152e5bb000f0acfd007c3d2192320462d4
LeviMatias <matilevi35@gmail.com>
2021-05-22 19:58:08 -0300

    changes to package de/serialization to support any file format

--- src/lib/package.py
+++ src/lib/package.py
@@ -1,4 +1,7 @@
 SEPARATOR = "|"
+SEPARATOR_ASCII = 124
+HEADER_END = "~"
+END_ASCII = 126
 
 
 class Header:
@@ -9,7 +12,7 @@ class Header:
         self.name = name
         self.filesz = filesz
         self.serialized = (f'{seqnum}'+SEPARATOR+f'{req}' + SEPARATOR +
-                           f'{name}'+SEPARATOR+f'{filesz}'+SEPARATOR)
+                           f'{name}'+SEPARATOR+f'{filesz}'+HEADER_END)
         self.size = len(self.serialized)
 
 
@@ -21,10 +24,25 @@ class Package:
 
     @staticmethod
     def deserialize(bytestream):
-        fields = bytestream.decode().split(SEPARATOR)
-        header = Header(int(fields[0]), fields[1], fields[2], int(fields[3]))
+        fields = []
 
-        return Package(header, fields[4].encode())
+        start = 0
+        end = 0
+        for elem in bytestream:
+            if elem == SEPARATOR_ASCII or elem == END_ASCII:
+                fields.append(bytestream[start:end])
+                start = end + 1
+            if elem == END_ASCII:
+                fields.append(bytestream[start:])
+                break
+            end += 1
+
+        header = Header(int(fields[0]),  # seqnum
+                        fields[1].decode(),  # request type
+                        fields[2].decode(),  # filename
+                        int(fields[3]))  # filesize
+
+        return Package(header, fields[4])
 
     @staticmethod
     def create_ack(num):  # todo recv ok_ack as param?
