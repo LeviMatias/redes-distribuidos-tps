895e09f84c3cd79a3984512ff688e69c21e65ceb
axel <axelmpm@gmail.com>
2021-07-06 16:39:20 -0300

    topo1 construida con spanning tree anda pingall

--- tp3-top-1/README.md
+++ tp3-top-1/README.md
@@ -1,1 +1,3 @@
+sudo ./pox.py forwarding.l2_learning openflow.spanning_tree --no-flood --hold-down openflow.discovery host_tracker
+
 sudo mn --custom topology1.py --topo topology1,switch_level=3,host_number=4 --mac --arp --switch ovsk --controller remote
--- tp3-top-1/topology1.py
+++ tp3-top-1/topology1.py
@@ -1,6 +1,14 @@
 from mininet.topo import Topo
 import math
 
+def get_host(host_number):
+    
+    if host_number < 10:
+        mac_host_number = '0'+str(host_number)
+    else:
+        mac_host_number = str(host_number)
+
+    return 'h'+str(host_number), '10.0.0.'+str(host_number), '00:00:00:00:00:'+mac_host_number
 
 def get_switch_amount(switch_level):
     sum = math.pow(2, switch_level - 1)
@@ -9,55 +17,72 @@ def get_switch_amount(switch_level):
         switch_level -= 1
     return int(sum)
 
-
 class Topology1(Topo):
 
+    def build( self, switch_level, host_amount):
 
-    def __init__( self, half_ports = 2, **opts ):
-        Topo.__init__(self, **opts)
+        if host_amount > 4:
+            host_amount = 4
 
-        switch_level = opts['switch_level']
-        host_number = opts['host_number']
+        if host_amount < 1:
+            host_amount = 1
 
-        switches = self.create_switches(switch_level)
+        if switch_level > 3:
+            switch_level = 3
 
-        self.add_switch_links(switches, switch_level)
-        self.add_hosts(switches)
+        if switch_level < 1:
+            switch_level = 1
 
+        self.switch_counter = 1
 
-    def create_switches(self, switch_level):
-        switch_amount = get_switch_amount(switch_level)
-        switch_number = 1
-        switches = []
-        for i in range(1, switch_amount + 1):
-            new_switch = self.addSwitch('sw' + str(switch_number))
-            switches.append(new_switch)
-            switch_number += 1
-        return switches
+        network_left_switch, network_right_switch = self.make_network(switch_level)
+        hosts = self.make_hosts(host_amount)
 
+        # connect hosts to network
+        self.connect_evenly_to_endpoints(hosts, network_left_switch, network_right_switch)
+  
+    def make_network(self, level=1):
 
-    def add_switch_links(self, switches, switch_level):
-        switch_counter = 0
-        for i in range(0, switch_level - 1):
-            for j in range(0, int(math.pow(2, i))):
-                switch_counter += 1
-                print("switch_counter", switch_counter)
-                print("first", switch_counter * 2)
-                print("second", switch_counter * 2 + 1)
-                self.addLink(switches[switch_counter - 1], switches[switch_counter * 2 - 1])
-                #self.addLink(switches[switch_counter - 1], switch_counter * 2)
+        if level==1:
+            only_switch = self.create_switch()
+            left_switch = only_switch
+            right_switch = only_switch
+        else:
+            left_switch = self.create_switch()
+            right_switch = self.create_switch()
 
-        for i in range((switch_level - 1), 0, -1):
-            level_switch = int(math.pow(2, i))
-            for j in range(0, level_switch):
-                switch_counter += 1
-                print("switch_counter", switch_counter)
-                print("first", switch_counter + (level_switch - j) + math.floor(j / 2))
-                self.addLink(switches[switch_counter - 1], switches[switch_counter + (level_switch - j) + math.floor(j / 2) - 1])
-        
+            # recursive magic
+            network_left_switch1, network_right_switch1 = self.make_network(level-1)
+            network_left_switch2, network_right_switch2 = self.make_network(level-1) 
 
-    def add_hosts(self, switches):
-        print("Add hosts")
+            nodes = [left_switch, right_switch]
+            self.connect_evenly_to_endpoints(nodes, network_left_switch1, network_right_switch1)
+            self.connect_evenly_to_endpoints(nodes, network_left_switch2, network_right_switch2)
 
+        return left_switch, right_switch
 
-topos = {'topology1': Topology1}
+    def create_switch(self):
+        switch = self.addSwitch("s"+str(self.switch_counter))
+        self.switch_counter += 1
+        return switch
+
+    def connect_evenly_to_endpoints(self, nodes, left_end_point, right_end_point):
+        for i, node in enumerate(nodes):
+            if i % 2 == 0:
+                self.addLink(node, left_end_point)
+            else:
+                self.addLink(node, right_end_point)
+
+    def make_hosts(self, amount):
+        hosts = []
+        for i in range(1, amount+1):
+            h, ip, mac = get_host(i)
+            hosts.append(self.addHost( h, ip=ip, mac=mac))
+        return hosts
+
+def start(switch_level=3, host_amount=4):
+    return Topology1(switch_level, host_amount)
+
+topos = {'topology1': start}
+
+start(3,4)
